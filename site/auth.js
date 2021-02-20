2b39f6dc94153711b8e8a9b4285293d666653dbb
ParadoxicalLife <71739913+ParadoxicalLife@users.noreply.github.com>
2021-02-20 14:41:37 -0500

    Signup, Login, and Logout functionality added

---
+++ site/auth.js
@@ -0,0 +1,57 @@
+// retrieve data
+/*db.collection*/
+
+// change states w/ login status
+auth.onAuthStateChanged(user => {
+    if (user) {
+        console.log('Current User: ', user);
+        setupUI(user);
+    } else {
+        console.log('Not logged in');
+        setupUI();
+    }
+})
+
+// account creation
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // store input
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    // ship to Firebase Auth
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user)
+        const modal = document.querySelector('#modal-signup');
+        // close form and reset fields after submission
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    })
+})
+
+// sign out
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+})
+
+// sign in 
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // store input
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    // ship to firebase auth
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        const modal = document.querySelector('#modal-login');
+        // close form and reset fields after submission
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    })
+})
