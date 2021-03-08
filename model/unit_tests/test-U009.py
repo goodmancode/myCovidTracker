e6311b85abba5274e4a5569474a89859c129614f
Sairam Nalla <51031586+Sairam-Nalla@users.noreply.github.com>
2021-03-07 20:05:55 -0500

    Add files via upload
    this is for sprint 5

---
+++ model/unit_tests/test-U009.py
@@ -0,0 +1,43 @@
+import unittest
+from StateMetrics import StateMetrics
+from RiskAssessment import RiskAssessment
+
+class RiskAssessmentTest(unittest.TestCase):
+
+    def test_set_age(self):
+        test = RiskAssessment(20, True, True, True, 25)
+        self.assertEqual(test.age, 20, 'Should be 20')
+        test.set_age(50)
+        self.assertEqual(test.age, 50, 'Should be 50')
+        test.set_age('thirty')
+        self.assertRaises(Exception)
+
+    def test_set_immuno_compromised(self):
+        test = RiskAssessment(20, True, True, True, 25)
+        self.assertEqual(test.immuno_compromised, True, 'Should be True')
+        test.set_immuno_compromised(False)
+        self.assertEqual(test.immuno_compromised, False, 'Should be False')
+        test.set_immuno_compromised(55)
+        self.assertRaises(Exception)
+
+    def test_calculate_risk(self):
+        s = StateMetrics([100, 120], 1, 150, 0.99, 0.25, 150)
+        calcRisk = RiskAssessment(20, True, True, True, 1)
+        self.assertEqual(calcRisk.calculate_risk(s), 1, "Should be 1")
+    
+    def test_set_environment(self):
+        test = RiskAssessment(20, True, True, True, 25)
+        self.assertEqual(test.environment, True, 'Should be True')
+        test.set_environment(False)
+        self.assertEqual(test.environment, False, 'Should be False')
+        test.set_environment(55)
+        self.assertRaises(Exception)
+        
+    def test_set_vaccinated(self):
+        test = RiskAssessment(20, True, True, True, 25)
+        self.assertEqual(test.vaccinated, True, 'Should be True')
+        test.set_vaccinated(False)
+        self.assertEqual(test.vaccinated, False, 'Should be False')
+        test.set_vaccinated(55)
+        self.assertRaises(Exception)
+
