bed7da60fac67978a2cce84deef26f41a4a44e4f
Sairam Nalla <51031586+Sairam-Nalla@users.noreply.github.com>
2021-03-07 20:13:30 -0500

    Update test-U009.py

--- model/unit_tests/test-U009.py
+++ model/unit_tests/test-U009.py
@@ -9,16 +9,16 @@ class RiskAssessmentTest(unittest.TestCase):
         self.assertEqual(test.age, 20, 'Should be 20')
         test.set_age(50)
         self.assertEqual(test.age, 50, 'Should be 50')
-        test.set_age('thirty')
-        self.assertRaises(Exception)
+        with self.assertRaises(Exception):
+            test.set_age('thirty')
 
     def test_set_immuno_compromised(self):
         test = RiskAssessment(20, True, True, True, 25)
         self.assertEqual(test.immuno_compromised, True, 'Should be True')
         test.set_immuno_compromised(False)
         self.assertEqual(test.immuno_compromised, False, 'Should be False')
-        test.set_immuno_compromised(55)
-        self.assertRaises(Exception)
+        with self.assertRaises(Exception):
+            test.set_immuno_compromised(55)
 
     def test_calculate_risk(self):
         s = StateMetrics([100, 120], 1, 150, 0.99, 0.25, 150)
@@ -30,14 +30,16 @@ class RiskAssessmentTest(unittest.TestCase):
         self.assertEqual(test.environment, True, 'Should be True')
         test.set_environment(False)
         self.assertEqual(test.environment, False, 'Should be False')
-        test.set_environment(55)
-        self.assertRaises(Exception)
-        
+        with self.assertRaises(Exception):
+            test.set_environment(55)
+
     def test_set_vaccinated(self):
         test = RiskAssessment(20, True, True, True, 25)
         self.assertEqual(test.vaccinated, True, 'Should be True')
         test.set_vaccinated(False)
         self.assertEqual(test.vaccinated, False, 'Should be False')
-        test.set_vaccinated(55)
-        self.assertRaises(Exception)
+        with self.assertRaises(Exception):
+            test.set_vaccinated(55)
 
+if __name__ == '__main__':
+    unittest.main()
