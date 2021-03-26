739b78949811c4f1c217d4fb41d6f86a29fa5307
ParadoxicalLife <steve.jacques@outlook.com>
2021-03-26 14:06:17 -0400

    Update views.py

--- mycovidtracker/mycovidtracker/views.py
+++ mycovidtracker/mycovidtracker/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from . import submit_button
+from mycovidtracker.backend import send_risk_to_database
 
 def index(request):
 #  return HttpResponse('index')
@@ -10,10 +11,13 @@ def submit(request):
 	#uid = request.POST.get('submission')
 	#submit_button.button(uid)
 	
+	#print('starting')
 	if request.method == 'POST':
 		uid = request.POST.get('uid')
 		
-		submit_button.button(uid)
+		#submit_button.button(uid)
+		send_risk_to_database(uid)
+	#	print('done')
 		
 	return HttpResponse('')		
 	#return HttpResponse("""<html><script>window.location.replace('/');</script></html>""")
